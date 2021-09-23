88ed47139ecf11f874f235f89b98b2417fddbc56
rmxrmx <mrokas@protonmail.com>
2021-09-23 13:58:04 +0200

    Text lemmatization implemented

--- src/rokas/working_with_spacy.py
+++ src/rokas/working_with_spacy.py
@@ -4,11 +4,9 @@ import os
 
 nlp = spacy.load("en_core_web_sm")
 doc = nlp("This is an English text")
-print(type(doc))
 
-token = doc[1]
 
-print(dir(doc))
+# print(dir(doc))
 
 
 def corpus_loader(folder: str) -> List[str]:
@@ -23,5 +21,29 @@ def corpus_loader(folder: str) -> List[str]:
     return text_list
 
 
+def should_lemmatize(token):
+    types = ["VERB", "ADJ", "NOUN"]
+    if token.pos_ in types:
+        return True
+    return False
+
+
+def lemmatize_text(text):
+    new_text = ""
+    doc = nlp(text)
+    for token in doc:
+        if should_lemmatize(token):
+            new_text += token.lemma_ + " "
+    return new_text
+
+
 training_path = os.path.join("..", "..", "syllabus", "classes", "data", "train_corpus")
-print(corpus_loader(training_path))
+loaded_corpus = corpus_loader(training_path)
+
+# lemmatize texts
+# for idx, text in enumerate(loaded_corpus[:20]):
+#     lemmatized = lemmatize_text(text)
+#     loaded_corpus[idx] = lemmatized
+#
+# print(loaded_corpus[0])
+
