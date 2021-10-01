2e553e1c25b59d9c4017404328f9278a7b1f6cac
rmxrmx <mrokas@protonmail.com>
2021-10-01 10:40:02 +0200

    Correct code!

--- src/rokas/working_with_pytorch.py
+++ src/rokas/working_with_pytorch.py
@@ -8,25 +8,6 @@ dataset = load_dataset("glue", "sst2")
 # select the train split
 train = dataset["train"]
 
-print("Examining train set:")
-print(train)
-print(train.features)
-
-print("Information about the dataset:")
-print(train.info.description)
-print("Homepage")
-print(train.info.homepage)
-
-print("Examining sentence")
-print(type(train["sentence"]))
-print(type(train["sentence"][0]))
-
-print("Examining label")
-print(type(train["label"]))
-print(type(train["label"][0]))
-# set takes all the unique values
-print(set(train["label"]))
-
 print("A few samples:")
 for t in range(10):
     sent = train["sentence"][t]
@@ -46,22 +27,10 @@ def doc_freq(documents: List[List[str]]) -> dict:
     """
     Takes in a list of documents which each is a list of tokens and return a dictionary of frequencies for each token over all the documents. E.g. {"Aarhus": 20, "the": 2301, ...}
     """
-    # c = Counter()
-    # for l in documents:
-    #     c.update(l)
-    #
-    # return dict(c)
-    #
-    # alternative implementation
-    c = {}
+    c = Counter()
     for l in documents:
-        l_set = set(l)
-        for key in l_set:
-            if key in c:
-                c[key] += 1
-            else:
-                c[key] = 1
-    return c
+        c.update(set(l))
+    return dict(c)
 
 
 print(term_freq(["testing", "these", "tokens", "these"]))
